d0aaf8a12d44d4e9d4019ffe70e7b1b4976bd560
Lucas <lucas.sellis@gmail.com>
2026-02-10 09:27:24 -0300

    Adiciona Dockerfile e exceções globais

---
+++ src/main/java/com/lucassellis/agendadortarefas/infrastructure/exceptions/UnauthorizedException.java
@@ -0,0 +1,14 @@
+package com.lucassellis.bff_agendadordetarefas.infrastructure.exceptions;
+
+import javax.naming.AuthenticationException;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String mensagem) {
+        super(mensagem);
+    }
+
+    public UnauthorizedException(String mensagem, Throwable throwable){
+        super (mensagem, throwable);
+    }
+}
